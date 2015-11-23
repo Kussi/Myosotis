b780ea1ce8425105c6d127b44f21e766efaf9c70
kussi <markus.recher@students.fhnw.ch>
2015-11-23 15:48:48 +0100

    Inital import

---
+++ Assets/Scripts/main.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        InsertBoard();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void InsertBoard()
+    {
+        Application.LoadLevel(1);
+        Application.LoadLevelAdditive("GameBoard");
+    }
+}
