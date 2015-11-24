cae216b80684fdbf6b180a9ddc3eabc8ac983307
AniBir <anila@gmx.net>
2015-11-24 15:39:30 +0100

    Text auf Dice hinzugefügt und implementiert, Frame für Spielfelder hinzugefügt

--- Assets/Scripts/DiceCtrl.cs
+++ Assets/Scripts/DiceCtrl.cs
@@ -1,24 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DiceControlling : MonoBehaviour {
+public class DiceCtrl : MonoBehaviour {
+
+	public bool isClicked;
 	
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	Ray ray;
 	RaycastHit hit;
 	
 	// Update is called once per frame
 	void Update () {
-		bool Text1 = false;
 		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		if(Physics.Raycast(ray, out hit))
-		if(Input.GetMouseButtonDown(0)){
-			Text1 = true;
-			Debug.Log(Random.Range(1,7));
+		if(Physics.Raycast(ray, out hit)){
+			if(Input.GetMouseButtonDown(0)){
+				try {
+					gameObject.GetComponent<TextMesh>().text = "" + Random.Range(1,7);
+				} catch (UnityException e) {} 
+				//Debug.Log(Random.Range(1,7));
+			}
 		}
 	}
 }
