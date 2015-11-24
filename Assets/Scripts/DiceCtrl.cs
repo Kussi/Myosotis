30b7b36abb82048327e15002b93b9f7398234cb1
AniBir <anila@gmx.net>
2015-11-24 14:07:58 +0100

    DiceCtrl Script hinzugefügt

---
+++ Assets/Scripts/DiceCtrl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiceControlling : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	Ray ray;
+	RaycastHit hit;
+	
+	// Update is called once per frame
+	void Update () {
+		bool Text1 = false;
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out hit))
+		if(Input.GetMouseButtonDown(0)){
+			Text1 = true;
+			Debug.Log(Random.Range(1,7));
+		}
+	}
+}
